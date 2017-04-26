44005e4986a85cac490a185d3663ea267ad69662
Holden Wight <htwight@gmail.com>
2017-04-26 14:20:55 -0400

    Rename main class

--- src/KnightsTour.java
+++ src/KnightsTour.java
@@ -26,7 +26,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class Main extends Application
+public class KnightsTour extends Application
 {
     @Override
     public void start(Stage primaryStage)
